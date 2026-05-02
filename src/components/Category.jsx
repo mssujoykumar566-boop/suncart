a061931b6b50e02adbaec8032691bec86d1173c1
mssujoykumar566-boop <mssujoykumar566@gmail.com>
2026-05-02 14:43:04 +0600

    category json update

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import React from 'react';
 
 const Category = async () => {
-       const res = await fetch('https://suncart-smoky.vercel.app/data.json');
+       const res = await fetch('https://suncart-smoky.vercel.app/category.json');
     const categories = await res.json();
     // console.log(categories,'categories')
     
@@ -11,13 +11,13 @@ const Category = async () => {
        <div className="mb-5 flex flex-wrap justify-center gap-2 sm:gap-3 md:gap-4">
   {
     categories.map((category) => (
-   <Link  key={category.id} href={`?category=${category.category.toLowerCase()}`}>
+   <Link  key={category.id} href={`?category=${category.name.toLowerCase()}`}>
     <Button
     size='sm'
      
       className="px-4 py-2 text-sm sm:text-base bg-orange-500 text-white rounded-full hover:bg-orange-600 transition"
     >
-      {category.category}
+      {category.name}
     </Button>
    </Link>
   ))}
